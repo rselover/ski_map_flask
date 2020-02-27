1f3bfec37bc28d49ef0ae04416c9100c1e7fa53e
Rob Selover <rselover@users.noreply.github.com>
2020-02-27 10:24:26 -0700

    repl.it working version 2020-0227

--- app.py
+++ app.py
@@ -1,59 +1,57 @@
-#import pandas as pd
-#import geopandas as gpd
-#from shapely.geometry import Point
-#import sqlite3
-#import pyodbc
 import json
 import mysql.connector as sql
-
+import os
 from flask import Flask, render_template
 from flask import jsonify
-app= Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/kids_ski.geojson")
+@app.route('/mbkey/')
+def mbkey():
+  data=jsonify(os.getenv("MB"))
+  return data.get_data(as_text=True)
 
+@app.route('/') # this is the home page route
 def db_geojson():
-    # Will update with pyodbc
-    #sql_data=('kids_ski.db')
-    #conn=sqlite3.connect(sql_data)
-    server='skidata.coanuqqxa6px.us-west-2.rds.amazonaws.com'
-    port=3306
-    db='skidata'
-    usr='skiadmin'
-    pwd='RTtxdrNgw6tKNKQ'
-    conn=sql.connect(user=usr,password=pwd,host=server,database=db)
-#    conn_str='DRIVER={MySQL ODBC 3.51 Driver};SERVER='+server+';DATABASE='+database+';USER='+user+'PASSWORD='+pwd+'OPTION=3;'
-#    conn=pyodbc.connect(conn_str)
 
-    cursor=conn.cursor()
+    conn = sql.connect(user=os.getenv("USR"), password=os.getenv("PASS"), host=os.getenv("SERVER"), database= os.getenv("DB"))
+
+    cursor = conn.cursor()
 
     cursor.execute('select * from kids_ski')
-    result=cursor.fetchall()
+    result = cursor.fetchall()
 
-    feature_list=[]
+    feature_list = []
 
     for i in range(len(result)):
-        r_dict=[dict(zip([column[0] for column in cursor.description],result[i])) for x in list(range(len(result)))]
-        g_dict={}
-        g_dict['geometry']={"type":"Point","coordinates":[r_dict[i]['lon'],r_dict[i]['lat']]}
+        r_dict = [
+            dict(zip([column[0] for column in cursor.description], result[i]))
+            for x in list(range(len(result)))
+        ]
+        g_dict = {}
+        g_dict['geometry'] = {
+            "type": "Point",
+            "coordinates": [r_dict[i]['lon'], r_dict[i]['lat']]
+        }
         del r_dict[i]['lat']
         del r_dict[i]['lon']
-        g_dict['properties']=r_dict[i]
-        g_dict['type']="Feature"
+        g_dict['properties'] = r_dict[i]
+        g_dict['type'] = "Feature"
         feature_list.append(g_dict)
 
-    geojson={"type":"FeatureCollection","features":feature_list}
+    geojson = {"type": "FeatureCollection", "features": feature_list}
 
-    return jsonify(geojson)
-
-#Added per https://stackoverflow.com/questions/22181384/javascript-no-access-control-allow-origin-header-is-present-on-the-requested
+    data=jsonify(geojson)
+    return data.get_data(as_text=True)
+    
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+   
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080) # This line is required to run Flask on repl.it
